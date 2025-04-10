7f27bb69e4bfe4a6a0b18d7601720ab2769fb9f3
EullerVinicius <euller.vinicius25@gmail.com>
2025-04-10 13:19:37 -0300

    .

---
+++ ex006/index.js
@@ -0,0 +1,7 @@
+var vel= 20
+console.log (`a velocidade do seu carro é de ${vel} km/h`)
+if(vel > 60) {
+    console.log('você ultrapassou o limite de velocidade, MULTADO !')
+   
+}
+console.log('DIRIJA COM SEGURANÇA USE O CINTO!')
